024f528941a40fb34c7c6d26e30fd10e4bac6f8f
Viktor Savonin <viktor.savonin@meawallet.com>
2018-10-06 15:07:37 +0300

    Mocks

--- src/main/java/lv/javaguru/junit/workshop/section1/TaxCalculatorImpl.java
+++ src/main/java/lv/javaguru/junit/workshop/section1/TaxCalculatorImpl.java
@@ -1,10 +1,23 @@
 package lv.javaguru.junit.workshop.section1;
 
-public class TaxCalculatorImpl implements TaxCalculator {
+public class TaxCalculatorImpl {
 
-    @Override
-    public double calculateTax(double income) {
-        return 0.0;
+    private TaxRateFinder taxRateFinder;
+    private EmailSender emailSender;
+
+    public TaxCalculatorImpl(TaxRateFinder taxRateFinder,
+                             EmailSender emailSender) {
+        this.taxRateFinder = taxRateFinder;
+        this.emailSender = emailSender;
+    }
+
+    public double calculateTax(int year, double income) {
+        if (income > 20000) {
+            EmailInfo emailInfo = new EmailInfo(year, income);
+            emailSender.sendEmail(emailInfo);
+        }
+        double taxRate = taxRateFinder.getTaxRate(year);
+        return income * taxRate;
     }
 
 }
